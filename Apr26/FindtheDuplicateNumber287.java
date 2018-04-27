6351c4d51d3199620d490c820e322db794bf42f0
Zac <tclu82@uw.edu>
2018-04-26 19:47:44 -0700

    Update 287 with 2 methods

--- Apr26/FindtheDuplicateNumber287.java
+++ Apr26/FindtheDuplicateNumber287.java
@@ -1,5 +1,7 @@
 package Apr26;
 
+import java.util.Arrays;
+
 public class FindtheDuplicateNumber287
 {
     public FindtheDuplicateNumber287()
@@ -8,23 +10,27 @@ public class FindtheDuplicateNumber287
 
         int[] nums = {1,3,4,2,2};
 
-        System.out.println(findDuplicate(nums));
+        System.out.println(findDuplicateAsLinkedList(nums));
+
+        System.out.println(findDuplicateUsingSort(nums));
     }
 
-    private int findDuplicate(int[] nums)
+    private int findDuplicateAsLinkedList(int[] nums)
     {
         if (nums == null || nums.length == 0) return -1;
-
+        // Like LL
         int slow = nums[0], fast = nums[slow];
-
+        // Find the mid point (duplicated index or element)
         while (fast != slow)
         {
-            slow = nums[slow];
-
             fast = nums[nums[fast]];
-        }
-        fast = 0;
 
+            slow = nums[slow];
+        }
+        // Set fast = 0 to check again
+        // Last round could stop in index, not element
+        fast = 0;
+        // 1 iteration 1 time
         while (fast != slow)
         {
             fast = nums[fast];
@@ -32,5 +38,18 @@ public class FindtheDuplicateNumber287
             slow = nums[slow];
         }
         return fast;
+    }
+
+    private int findDuplicateUsingSort(int[] nums)
+    {
+        if (nums == null || nums.length == 0) return -1;
+        // Sorted
+        Arrays.sort(nums);
+
+        for (int i=0; i<nums.length -1; i++)
+        {
+            if (nums[i] == nums[i + 1]) return nums[i];
+        }
+        return -1;
     }
 }
