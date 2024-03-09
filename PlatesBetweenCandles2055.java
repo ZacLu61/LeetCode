08a7202f56f7b3cde3352436fa3aec0147913dfe
Zac Lu <zaclu61@gmail.com>
2024-03-08 21:11:08 -0800

    Add PlatesBetweenCandles2055

---
+++ PlatesBetweenCandles2055.java
@@ -0,0 +1,18 @@
+class PlatesBetweenCandles2055 {
+    
+    public int[] platesBetweenCandles(String s, int[][] queries) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // Test 1
+        String s = "**|**|***|";
+        int[][] queries = {{1,2},{3,4}};
+        // expect: {2,3}
+
+
+
+        System.out.println(new PlatesBetweenCandles2055().platesBetweenCandles(s, queries));
+    }
+}
